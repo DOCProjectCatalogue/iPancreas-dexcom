413d5def4290003af4e5d44fd5116538aef2d6ba
Jana E. Beck <jana.eliz.beck@gmail.com>
2014-05-19 01:18:00 -0700

    update example script to produce JSON output

--- example.py
+++ example.py
@@ -2,21 +2,36 @@
 # Contact: jana.eliz.beck@gmail.com
 # License: GPLv3 (http://www.gnu.org/licenses/gpl-3.0.txt)
 
+from __future__ import print_function
+
 from dexcom import merge_csv
+from dexcom.convert_to_JSON import DexcomJSON
 
 def main():
   # example usage of merge_csv via the dexcom module instead of via command-line args
+  example_output = 'example_output.csv'
+
   args = {
-    'csv': False,
-    'device_gen': False,
+    'csv': True,
+    'device_gen': True,
     # change me to the path (optional), filename and extension where you'd like the output saved
-    'output_file': '',
+    'output_file': example_output,
     # change me to the proper path to the directory where your Dexcom files are stored
     'dir_path': '',
-    'terse': False
+    'serial': True,
+    'terse': True
   }
 
   merge_csv.process(args)
+
+  with open(example_output, 'rU') as f:
+    dex = DexcomJSON(f, {
+      'format': 'tidepool',
+      'file': 'example-output.json'
+    })
+
+    # bloodhound takes a timezone string - e.g., 'US/Eastern' 
+    dex.bloodhound('').print_JSON()
 
 if __name__ == '__main__':
   main()
